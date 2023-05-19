1b7dad02943e7287c045f763ec552f1d0a6144c3
chebb <chebbi603@gmail.com>
2023-05-19 11:53:12 +0200

    final version

--- app/src/main/java/com/twensa/application/MainActivity.java
+++ app/src/main/java/com/twensa/application/MainActivity.java
@@ -1,14 +1,24 @@
 package com.twensa.application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ConstraintLayout c = (ConstraintLayout) findViewById(R.id.cons);
         setContentView(R.layout.activity_main);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "UNDER DEVELOPMENT", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
